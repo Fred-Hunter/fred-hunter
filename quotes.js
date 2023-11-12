11c311970dcb8a07a7e4f31665d1b1974681712b
Fred-Hunter <fredski.1+fredgithub@hotmail.co.uk>
2023-11-12 15:29:20 +0000

    Add quotes

---
+++ quotes.js
@@ -0,0 +1,47 @@
+const quotes = [{
+    text: "If you close your eyes how do you sleep?",
+    author: "Ethan Montgomery"
+},{
+    text: "If I was six how old was I?",
+    author: "Liam Anderson"
+},{
+    text: "Sorry I'm eating grapes",
+    author: "Ren Ishikawa"
+},{
+    text: "How does it exist if you can't see it",
+    author: "Ren Ishikawa"
+},{
+    text: "I know by boobies",
+    author: "Emma Sullivan"
+},{
+    text: "Do you want me to crochet it a face?",
+    author: "Ava Campbell"
+},{
+    text: "Sandwiches - fine. Fine. Deal with that.",
+    author: "Evelyn Cooper"
+},{
+    text: "It's like high jump but the bar gets higher and you've got to get over it?",
+    author: "Sebastian Cheshire"
+},{
+    text: "What is rizz?",
+    author: "Benedict Thornton"
+},{
+    text: "People often liken me to drinking a glass of water backwards",
+    author: "Emma Sullivan"
+},{
+    text: "Stuff can't happen simultaneously",
+    author: "Ethan Montgomery"
+},{
+    text: "I can answer this question, what was the question?",
+    author: "Reginald Middleton"
+},{
+    text: "You can never be wrong about stuff",
+    author: "Ethan Montgomery"
+}];
+
+const quote = quotes[Math.floor(Math.random() * quotes.length)]
+
+document.getElementsByClassName("text_quote")[0].textContent = quote.text;
+document.getElementsByClassName("text_quoteAuthor")[0].textContent = quote.author;
+
+document.getElementsByClassName("blob")[0].setAttribute("style", "filter: hue-rotate(" + Math.random() * 360 + "deg)");
